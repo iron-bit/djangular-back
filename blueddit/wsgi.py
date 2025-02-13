f404b7c37d413676a4c14636fbced0d1850b0b36
Tymur <tymurkulivar@gmail.com>
2025-02-13 20:19:11 +0100

    refactor project structute

--- blueddit/wsgi.py
+++ blueddit/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for djangular project.
+WSGI config for blueddit project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangular.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blueddit.settings')
 
 application = get_wsgi_application()
