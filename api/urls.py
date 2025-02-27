cc757f3a7dce70fcb345f0d218d8058bda627b36
ciovanes <iovanescuclaudiu@proton.me>
2025-02-27 13:29:17 +0100

    Add GET communities endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import UserRegistrationView, UserProfileView, UpdateAura, LogoutView, GetPostsView, PostCreateView
+# from .views import UserRegistrationView, UserProfileView, UpdateAura, LogoutView, GetPostsView, PostCreateView
+from .views import UserRegistrationView, UserProfileView, LogoutView, GetPostsView, PostCreateView, CommunitiesView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
@@ -11,8 +12,9 @@ urlpatterns = [
     path('profile/', UserProfileView.as_view(), name='profile'),
     # Para pedir el nuevo token, lo explicaré en clase.
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # Para crear un post 
+    # Para crear un post
     path('create_post/', PostCreateView.as_view(), name='post'),
     path('posts/', GetPostsView.as_view(), name='posts'),
-    path('aura/<int:post_id>/', UpdateAura.as_view(), name='vote_post'),
+    # path('aura/<int:post_id>/', UpdateAura.as_view(), name='vote_post'),
+    path('communities/', CommunitiesView.as_view(), name='communities-list')
 ]
